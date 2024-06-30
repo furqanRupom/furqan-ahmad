d01ce9b50566730fb971da28ca42d9778f5b7dd0
furqanrupom <furqanrupom978@gmail.com>
2024-06-30 15:04:59 +0600

    make responsive banner

--- src/components/home/banner/Banner.tsx
+++ src/components/home/banner/Banner.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { ArrowDownToLine } from 'lucide-react';
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import * as React from 'react';
 import { motion } from 'framer-motion';
 import { icons } from '@/config/icons';
@@ -10,7 +10,7 @@ interface IBannerProps { }
 const Banner: React.FunctionComponent<IBannerProps> = (props) => {
     return (
         <motion.section
-            className='min-h-screen bg-center lg:bg-top'
+            className='min-h-screen bg-center md:bg-top'
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.5 }}
@@ -19,11 +19,10 @@ const Banner: React.FunctionComponent<IBannerProps> = (props) => {
                 backgroundSize: 'cover',
             }}
         >
-            <div className='max-w-5xl mx-auto text-center px-4'>
-                <div className='flex flex-col items-center justify-center'>
+            <div className=' mx-auto text-center px-4'>
+                <div >
                     <motion.div
                         className='flex flex-col lg:flex-row items-center justify-center'
-                 
                     >
                         <Image
                             src='https://i.ibb.co/CB6CPZP/images-2-removebg-preview.png'
@@ -71,9 +70,9 @@ const Banner: React.FunctionComponent<IBannerProps> = (props) => {
                             </button>
                         </motion.div>
 
-                        <div className='relative mt-12'>
+                        <div className='relative mt-12  '>
                             <motion.div
-                                className='absolute inset-0'
+                                className=' absolute inset-0'
                                 initial={{ scale: 0.8, opacity: 0 }}
                                 animate={{ scale: 1, opacity: 1 }}
                                 transition={{ duration: 0.5 }}
@@ -86,75 +85,23 @@ const Banner: React.FunctionComponent<IBannerProps> = (props) => {
                                     alt='logo'
                                 />
                             </motion.div>
-
-                            <div className='absolute top-12 lg:top-20 w-full'>
+                            <div className='absolute top-12 lg:top-20 w-full px-4'>
                                 {/* first two icons */}
-                                <div className='flex justify-between w-full max-w-lg mx-auto'>
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.1 }}
-                                    >
-                                        <Image src={icons.nextjs} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>Next JS</h3>
-                                    </motion.div>
-
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.2 }}
-                                    >
-                                        <Image src={icons.react} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>React</h3>
-                                    </motion.div>
+                                <div className='flex justify-between w-full max-w-md lg:max-w-lg mx-auto'>
+                                    <IconCard src={icons.nextjs} alt='Next JS logo' label='Next JS' delay={0.1} />
+                                    <IconCard src={icons.react} alt='React logo' label='React' delay={0.2} />
                                 </div>
 
                                 {/* middle two icons */}
-                                <div className='flex justify-between w-full mt-6 md:mt-16 '>
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.3 }}
-                                    >
-                                        <Image src={icons.postgres} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>PostgresSQL</h3>
-                                    </motion.div>
-
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.4 }}
-                                    >
-                                        <Image src={icons.reactquery} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>React Query</h3>
-                                    </motion.div>
+                                <div className='flex justify-between w-full mt-16 max-w-lg lg:max-w-2xl mx-auto'>
+                                    <IconCard src={icons.postgres} alt='PostgresSQL logo' label='PostgresSQL' delay={0.3} />
+                                    <IconCard src={icons.reactquery} alt='React Query logo' label='React Query' delay={0.4} />
                                 </div>
 
                                 {/* last two icons */}
-                                <div className='flex justify-between w-full mt-6 md:mt-16 lg:max-w-4xl'>
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.5 }}
-                                    >
-                                        <Image  src={icons.tailwindcss} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>TailwindCSS</h3>
-                                    </motion.div>
-
-                                    <motion.div
-                                        className='bg-white flex border border-red-400 w-fit items-center p-2 rounded-full gap-3'
-                                        initial={{ opacity: 0, y: 20 }}
-                                        animate={{ opacity: 1, y: 0 }}
-                                        transition={{ duration: 0.5, delay: 0.6 }}
-                                    >
-                                        <Image src={icons.mongodb} width={40} height={40} className='w-8 h-8' alt='logo' />
-                                        <h3 className='text-gray-900 text-xs sm:text-base lg:text-xl font-bold'>MongoDB</h3>
-                                    </motion.div>
+                                <div className='flex justify-between w-full mt-16 max-w-xl mx-auto'>
+                                    <IconCard src={icons.tailwindcss} alt='TailwindCSS logo' label='TailwindCSS' delay={0.5} />
+                                    <IconCard src={icons.mongodb} alt='MongoDB logo' label='MongoDB' delay={0.6} />
                                 </div>
                             </div>
                         </div>
@@ -166,3 +113,16 @@ const Banner: React.FunctionComponent<IBannerProps> = (props) => {
 };
 
 export default Banner;
+
+
+const IconCard = ({ src, alt, label, delay }:{src:StaticImageData,alt:string,label:string,delay:any}) => (
+    <motion.div
+        className='bg-white flex border border-red-400 items-center p-2 rounded-full gap-3 transition-all duration-300 hover:shadow-lg'
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay }}
+    >
+        <Image src={src} width={40} height={40} className='lg:w-8 lg:h-8 w-5 h-5' alt={alt} />
+        <h3 className='text-gray-900 text-xs md:text-base lg:text-xl font-bold'>{label}</h3>
+    </motion.div>
+)
--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
     return (
         <nav className='fixed top-0 w-full '>
             <motion.div
-                className='max-w-7xl mx-auto p-4 lg:p-2 lg:rounded-3xl flex justify-between lg:my-5 bg-red-50 dark:bg-red-50 dark:bg-opacity-5 shadow'
+                className='max-w-7xl mx-auto p-4 lg:p-2 lg:rounded-3xl flex justify-between lg:my-5 bg-white dark:bg-red-50 dark:bg-opacity-5 bg-opacity-5 shadow backdrop-blur-3xl'
                 initial={{ y: -100 }}
                 animate={{ y: 0 }}
                 transition={{ duration: 0.5 }}
