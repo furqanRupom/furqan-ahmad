d36e95acf2281a1899c1f758aa6ad5a552fd8c78
furqanrupom <furqanrupom978@gmail.com>
2024-07-01 12:48:41 +0600

    set up databse and back end

--- src/app/(dashboardLayout)/dashboard/page.tsx
+++ src/app/(dashboardLayout)/dashboard/page.tsx
@@ -5,7 +5,13 @@ import HomePage from '@/components/dashboard/home/HomePage';
 interface IdashboardHomeProps {
 }
 
-const dashboardHome: React.FunctionComponent<IdashboardHomeProps> = (props) => {
+const dashboardHome: React.FunctionComponent<IdashboardHomeProps> = async (props) => {
+
+  const data = await fetch('http://localhost:3000/api/blogs',{
+    method:"GET"
+  })
+  const response = await data.json();
+  console.log(response);
   return <><HomePage /></>
 };
 
